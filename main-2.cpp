c5fbb51853c0845491a4a8b3985463438077284d
NastassiaShypionak <157250312+NastassiaShypionak@users.noreply.github.com>
2024-02-12 16:25:48 -0500

    Add files via upload

---
+++ main-2.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+Falling distance 
+Nastassia Shypionak
+01/22/2024
+*******************************************************************************/
+#include <iostream>
+#include <cmath>    // for pow() function
+using namespace std;
+
+const double g = 9.8; 
+
+double fallingDistance(int);
+
+int main()
+{
+	double d; //distance
+    double t; //time
+
+	for (int t = 1; t <= 10; t++)
+    {
+        d = fallingDistance(t);
+        
+        cout <<"For "<< t << " seconds the distance is "
+             << d << " meters"
+             << endl;
+    }
+	
+  	return 0;
+}
+
+double fallingDistance(int t)
+{
+    return (0.5) * g * pow(t, 2);
+}
+
